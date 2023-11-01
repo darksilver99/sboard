be784a4d969a7344756f249e768b3caa73b12b7b
Darksilver88 <anusorn@koder3.com>
2023-11-01 17:41:22 +0700

    a

--- lib/custom_code/actions/del_file_from_a_p_i.dart
+++ lib/custom_code/actions/del_file_from_a_p_i.dart
@@ -7,9 +7,20 @@ import 'package:flutter/material.dart';
 // Begin custom action code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
 
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
 Future delFileFromAPI(
   String? uploadKey,
   String? path,
 ) async {
   // Add your function code here!
+  print("delFileFromAPI");
+  var url = Uri.https('www.silver-api.com', 'webboard/App_api_v1/delFile');
+  var data = {
+    "uploadKey": uploadKey,
+    "path": path,
+  };
+
+  await http.post(url, body: data);
 }
